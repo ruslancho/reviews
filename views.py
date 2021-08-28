23d7232e472a41348638ab8489afbce98aaf979f
Ruslancho <ruslancho@ukr.net>
2021-08-28 14:25:28 +0300

    0.1

--- views.py
+++ views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("<h3>Привіт світ!</h3>")
+    full_path = request.get_full_path().split('/')[2].split('_')
+    print(full_path)
+    with open("reviews.txt", "a") as f:
+        f.write(full_path[0] + ':' + full_path[1] + '\n')
+    return HttpResponse("<h3>Дякуємо за відгук!</h3>")
