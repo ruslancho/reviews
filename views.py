0e7e281c920586bafdff3c105109329c4e14822f
Ruslancho <ruslancho@ukr.net>
2021-09-01 16:49:46 +0300

    0.1

--- views.py
+++ views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import pyodbc
 
 def index(request):
     full_path = request.get_full_path().split('/')[2].split('_')
     print(full_path)
+    server = 'tcp:KAU-LT-000475'
+    database = 'reviews'
+    username = 'sa'
+    password = '6nY69p9ULm'
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    cursor.close()
     with open("reviews.txt", "a") as f:
         f.write(full_path[0] + ':' + full_path[1] + '\n')
     return HttpResponse("<img src='image.jpg'>")
